73a133a2489788888e47422b8e0b67f230184314
SarveshPanwar <94594566+SarveshPanwar@users.noreply.github.com>
2025-07-17 14:43:04 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,28 @@
 "use strict";  // treat all JS code as newer version
 
-alert( 3 + 3)
+// alert( 3 + 3)  // we are using nodejs , not browser
+
+console.log(3
+    +3)   // bad readability
+
+console.log("shanu")
+
+
+let name = "Sarvesh"
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number => 2 to the power of 53
+// bigint 
+// string = ""
+// boolean = true/false
+// null => standlone value
+// undefined => value not assigned 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined);  // undeifined 
+console.log(typeof null);  // object
