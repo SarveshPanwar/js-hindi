adbb7c4e4f8fa7996f22f1e8d0472fb83c1af022
SarveshPanwar <94594566+SarveshPanwar@users.noreply.github.com>
2025-07-17 18:29:17 +0000

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "hitesh"
+let score = "Sarvesh"
 
 // console.log(typeof score);
 // console.log(typeof(score)); // another way
@@ -29,5 +29,49 @@ let BooleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************* Operations ************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log (2+2);
+// console.log (2-2);
+// console.log (2*2);
+// console.log (2**2);
+// console.log (2/2);
+// console.log (2%3);
+
+let str1 = "Hello"
+let str2 = " Sarvesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3 = 4
+
+
+// Postfix Operator (x++)
+// -> increments the value but returns the value BEFORE increment
+let x = 3;
+const y = x++;
+// console.log( x, y);
+
+// Prefix Operator (++x)
+// -> increments the value and returns the value AFTER increment
+let a = 3;
+const b = ++a;
+console.log( a, b);
+ 
