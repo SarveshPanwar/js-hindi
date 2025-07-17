14313cf967fc2f4ba260ecbb0933bbd966b7e4b2
SarveshPanwar <94594566+SarveshPanwar@users.noreply.github.com>
2025-07-17 15:10:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof(score)); // another way
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// Notes on conversion
+// "33" => 33
+// "33abc" => Nan (not a number)
+// true => 1
+// false => 0
+
+
+let isLoggedIn = ""
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "sarvesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
